ae5a84e797db5310470aca7c6c7d275c88dbcb97
Vinod <vtkrishn@gmail.com>
2018-10-19 18:39:42 -0700

    changes

--- build.sh
+++ build.sh
@@ -1,12 +1,7 @@
 #! /bin/sh
 
-parameter=$1
 git pull
-npm test
-npm run cover
-npm version patch -m "Version %s - $parameter"
 
 git add .
-git commit -m "$parameter"
-git push && git push --tags
-npm publish
+git commit -m "changes"
+git push
